f215a8f07f9eab4ea99afc9d2e93514a099037e5
realmadsci <6604845-realmadsci@users.noreply.gitlab.com>
2022-02-08 12:27:54 -0500

    Refactor

--- rrod_proxy.py
+++ rrod_proxy.py
@@ -1,15 +1,6 @@
 # Required packages:
 # `python -m pip install trio colorama termcolor`
-
-
-# If you want to use IPv6, then:
-# - replace AF_INET with AF_INET6 everywhere
-# - use the hostname "2.pool.ntp.org"
-#   (see: https://news.ntppool.org/2011/06/continuing-ipv6-deployment/)
-
-import datetime
 import msvcrt
-import struct
 
 from colorama import init
 from termcolor import cprint
@@ -18,17 +9,56 @@ import trio
 # Set up terminal coloring for Windows
 init()
 
+
+async def handle_keypress(key, client, server):
+    """
+    Handle keypresses
+
+    NOTE: This is only called when the proxy is up and connected, so the client and server sockets should
+          always be valid capable of sending data inside this function.
+
+    NOTE: The Esc key b"\x1b" and Ctrl-C b"\x03" are already taken to mean "exit" so you'll never see them in here!
+    """
+    cprint(f"Got key: '{key}'", "yellow")
+
+
+async def handle_client_to_server_data(data, client, server):
+    """
+    Handle data that has arrived from the client and is heading to the server.
+
+    You can either do the simple "send it along" thing like so:
+    `await server.send(data)`
+    or you can do fancier things, even including spoofing data back to the client if you wish.
+    """
+    cprint("Client ---> Server\n" + data.hex(" "), "green")
+    await server.send(data)
+
+
+async def handle_server_to_client_data(data, client, server):
+    """
+    Handle data that has arrived from the server and is heading to the client.
+
+    You can either do the simple "send it along" thing like so:
+    `await client.send(data)`
+    or you can do fancier things, even including spoofing data back to the server if you wish.
+    """
+    cprint("Client <--- Server\n" + data.hex(" "), "blue")
+    await client.send(data)
+
+
 class TcpProxy:
     def __init__(self, client_port, server_ip, server_port):
         self.client_port = client_port
         self.server_ip = server_ip
         self.server_port = server_port
+        self.client_conn = None
+        self.server_conn = None
 
     async def run(self):
         # Create a TCP socket to listen locally
         with trio.socket.socket(
-            family=trio.socket.AF_INET,     # IPv4
-            type=trio.socket.SOCK_STREAM,   # TCP
+            family=trio.socket.AF_INET,  # IPv4
+            type=trio.socket.SOCK_STREAM,  # TCP
         ) as client_sock:
             # NOTE: This binds to ONLY the localhost interface, so nothing outside of this local host can connect to it!
             await client_sock.bind(("127.0.0.1", self.client_port))
@@ -36,38 +66,62 @@ class TcpProxy:
 
             # This loop will reconnect things whenever the client socket drops, so we don't have to keep restarting this proxy for every connection:
             while True:
-                (conn, addr) = await client_sock.accept()
+                (self.client_conn, addr) = await client_sock.accept()
                 # NOTE: We *could* spawn off multiple child tasks here and open multiple parallel connections with accept() but we _don't_ for
                 # simplicity and because the goal of this proxy is to study/manipulate the connection (rather than actually be a proxy):
-                with conn:
-                    print(f"Connected by {addr}, connecting to {self.server_ip}:{self.server_port}")
+                with self.client_conn:
+                    print(
+                        f"Connected by {addr}, connecting to {self.server_ip}:{self.server_port}"
+                    )
                     with trio.socket.socket(
-                        family=trio.socket.AF_INET,     # IPv4
-                        type=trio.socket.SOCK_STREAM,   # TCP
-                    ) as server_sock:
-                        await server_sock.connect((self.server_ip, self.server_port))
+                        family=trio.socket.AF_INET,  # IPv4
+                        type=trio.socket.SOCK_STREAM,  # TCP
+                    ) as self.server_conn:
+                        await self.server_conn.connect(
+                            (self.server_ip, self.server_port)
+                        )
 
-                        async def server_recv_loop():
+                        async def server_recv_loop(cancel_scope):
                             # Loop forever, sending received traffic to the client
-                            while True:
-                                data = await server_sock.recv(4096)
-                                if not data:
-                                    break
-                                cprint("Client <--- Server\n" + data.hex(' '), 'red')
-                                await conn.send(data)
+                            try:
+                                while True:
+                                    data = await self.server_conn.recv(4096)
+                                    if not data:
+                                        cprint(f"Server disconnected.", "red")
+                                        break
 
-                        async def client_recv_loop():
+                                    await handle_server_to_client_data(
+                                        data, self.client_conn, self.server_conn
+                                    )
+
+                            except Exception as e:
+                                cprint(f"Client <--- Server exception: {e}", "red")
+
+                            # Close down all tasks
+                            cancel_scope.cancel()
+
+                        async def client_recv_loop(cancel_scope):
                             # Loop forever, sending received traffic to the server
-                            while True:
-                                data = await conn.recv(4096)
-                                if not data:
-                                    break
-                                cprint("Client ---> Server\n" + data.hex(' '), 'green')
-                                await server_sock.send(data)
+                            try:
+                                while True:
+                                    data = await self.client_conn.recv(4096)
+                                    if not data:
+                                        cprint(f"Client disconnected.", "red")
+                                        break
+
+                                    await handle_client_to_server_data(
+                                        data, self.client_conn, self.server_conn
+                                    )
+
+                            except Exception as e:
+                                cprint(f"Client ---> Server exception: {e}", "red")
+
+                            # Close down all tasks
+                            cancel_scope.cancel()
 
                         async with trio.open_nursery() as nursery:
-                            nursery.start_soon(server_recv_loop)
-                            nursery.start_soon(client_recv_loop)
+                            nursery.start_soon(server_recv_loop, nursery.cancel_scope)
+                            nursery.start_soon(client_recv_loop, nursery.cancel_scope)
 
 
 # THIS IS WINDOWS-ONLY!
@@ -81,21 +135,32 @@ async def getch_iterator():
         if ch[0] == 0 or ch[0] == 0xE0:
             ch2 = await trio.to_thread.run_sync(msvcrt.getch, cancellable=True)
             # Prefix 0xE0 to the front.
-            ch = b'\xE0' + ch2
+            ch = b"\xE0" + ch2
         yield ch
 
+
 async def main():
-    #tcp_proxy = TcpProxy(prt, "remote_ip", prt)
+    # tcp_proxy = TcpProxy(prt, "ip", prt)
 
-    # Read responses from the socket.
-    with trio.move_on_after(1000):
-        async with trio.open_nursery() as nursery:
+    async with trio.open_nursery() as root_nursery:
 
-            nursery.start_soon(tcp_proxy.run)
+        # Run the TCP proxy:
+        root_nursery.start_soon(tcp_proxy.run)
 
-            async for key in getch_iterator():
-                print(f"Got key: '{key}'")
-                if key == b'q':
-                    nursery.cancel_scope.cancel()
+        # Handle keypresses
+        async for key in getch_iterator():
+            if key == b"\x1b" or key == b"\x03":
+                cprint(f"Received escape key. Exiting...", "red")
+                root_nursery.cancel_scope.cancel()
+
+            # Snooping into `_sock._closed` private variable is sketch, but :shrug:
+            elif (
+                tcp_proxy.client_conn is not None
+                and not tcp_proxy.client_conn._sock._closed
+                and tcp_proxy.server_conn is not None
+                and not tcp_proxy.server_conn._sock._closed
+            ):
+                await handle_keypress(key, tcp_proxy.client_conn, tcp_proxy.server_conn)
+
 
 trio.run(main)
